5008f05ae3f334f3bee24e919e245ea9b932e7e4
Eran <Eran@DESKTOP-ILE3GDK>
2018-04-13 18:53:56 +0300

    gfggfgfgfgfgfgf

--- ProjectPartB/src/Test.java
+++ ProjectPartB/src/Test.java
@@ -2,9 +2,5 @@
 public class Test {
 	public static void main(String[] args)
 	{
-		System.out.println("DAMN!!!!");
-		System.out.println("DAMN!!!!");
-		System.out.println("DAMN!!!!");
-		System.out.println("DAMN!!!!");
 	}
 }
